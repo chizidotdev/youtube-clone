453e56c9bbdf806cd5349b6656af1eb6d0d61a19
chiziivictor <chizivwokoma@gmail.com>
2022-07-21 21:25:26 +0100

    register and sign in controllers

--- api/src/controllers/auth.js
+++ api/src/controllers/auth.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
@@ -17,15 +19,25 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   try {
-    const user = await newUser.findOne({ name: req.body.name });
+    const user = await User.findOne({ name: req.body.name });
     if (!user) return next(createError("User not found", 404));
 
     // bcrypt compare password
-    const isValid = bcrypt.compare(req.body.password, user.password);
+    const isValid = await bcrypt.compare(req.body.password, user.password);
     if (!isValid) return next(createError("Invalid credentials", 401));
 
     // create token
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    // remove password from user object
+    const { password, ...userWithoutPassword } = user.toObject();
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(userWithoutPassword);
   } catch (err) {
     next(err);
   }
